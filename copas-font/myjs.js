af032585f1bb848b2a3e521905b5925c044f9354
Gchan1319 <83627675+Gchan1319@users.noreply.github.com>
2021-05-22 11:01:21 +0700

    Add files via upload

--- copas-font/myjs.js
+++ copas-font/myjs.js
@@ -1,15 +1,2 @@
-function myf(){
-     var x = setInterval(function() {
-          const i = document.getElementById('input').value
-          homeArticle.innerHTML=('Jika ada yang masih kurang untuk tulisan <p>['+i+']</p> yang anda ketik silahkan hubungi <a href="https://wa.me/+6285895811075">owner</a> :)')
-          console.log("interval on")
-          
-      }, 10);
-     var y = setInterval(function() {
-          if(i!=""){
-              clearInterval(x)
-              console.log("interval off")
-              clearInterval(y)
-          }
-      }, 5000);
-}
+function myf(){var x=setInterval(function(){const i=document.getElementById('input').value;if(i==""){homeArticle.innerHTML=""}else if(i!=""){homeArticle.style.display="block";homeArticle.innerHTML=(`Jika ada yang masih kurang untuk tulisan <p>[${i}]</p> \nyang anda ketik silahkan hubungi <a href="https://wa.me/6285895811075?text=Hi Mr.ParadoX 👋%0Atolong tambahin font buat text ini.%0ANama Font : (isi)%0ATeks : ${encodeURI(i)}">owner</a> :v`);console.log("interval on")}},10);var y=setInterval(function(){if(i!=""){clearInterval(x);console.log("interval off");clearInterval(y)}},8000)}
+function callside(){alert('sukses')}
