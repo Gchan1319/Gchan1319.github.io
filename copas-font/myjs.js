05829d2dbd6cc3dcc8e10c9dd8c9d9beaaf80a20
Gchan1319 <83627675+Gchan1319@users.noreply.github.com>
2021-05-23 13:08:15 +0700

    Add files via upload

--- copas-font/myjs.js
+++ copas-font/myjs.js
@@ -1,16 +1,16 @@
 function myf(){
   var x=setInterval(function(){
-  const i=document.getElementById('input').value;
+    let i=document.getElementById('input').value;
   if(i==""){
     homeArticle.innerHTML=""
   }else if(i!=""){
     homeArticle.style.display="block";
-    homeArticle.innerHTML=(`Jika ada yang masih kurang untuk tulisan <p>[${i}]</p> \nyang anda ketik silahkan hubungi <a href="https://wa.me/6285895811075?text=Hi Mr.ParadoX 👋%0Atolong tambahin font buat text ini.%0ANama Font : (isi)%0ATeks : ${encodeURI(i)}">owner</a> :v`)
+    homeArticle.innerHTML=(`Jika ada yang masih kurang untuk tulisan [<p>${i}</p>] \nyang anda ketik silahkan hubungi <a href="https://wa.me/6285895811075?text=Hi Mr.ParadoX 👋%0Atolong tambahin font buat text ini.%0ANama Font : (isi)%0ATeks : ${encodeURI(i)}">owner</a> :v`)
     console.log("interval on")
     }
-  }, 10)
+  }, 500);
     var y=setInterval(function(){
       if(i!=""){
         clearInterval(x);console.log("interval off");clearInterval(y)}
-    },8000)
-  }
+    },100000)
+}
