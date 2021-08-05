9890600b9f7a0bf8782a8ed80613235c3dbaedb2
Gchan1319 <83627675+Gchan1319@users.noreply.github.com>
2021-08-05 15:35:38 +0700

    Add files via upload

--- covid/main.js
+++ covid/main.js
@@ -1,10 +1,11 @@
- item = ['things','america','india','indonesia','malaysia','japan','South Korea','brazil','italy','saudi arabia','Australia','South africa','Armenia','belgium','Austria','Bangladesh','philippnes','Georgia','Hungary','Iraq','Iran','ireland','iceland','israel','canada','Kazakhstan','kenya','colombia','Costa rica','Morocco','Mexico','rusia','peru','chile','pakistan','turki','perancis','jerman','qatar','Venezuela','vietnam','myanmar','Papua New Guinea','taiwan','yemen','new Zealand','nepal','Madagascar','thailand','mali','paraguay','Luksemburg','palestine','Latvia','jordan','somalia','Equatorial Guinea','Nicaragua','maldives','cuba','Mozambique','germany','spanyol','laos','barbados']
+ item = ['things','america','india','indonesia','malaysia','japan','South Korea','brazil','italy','saudi arabia','Australia','South africa','Armenia','belgium','Austria','Bangladesh','philippnes','Georgia','Hungary','Iraq','Iran','ireland','iceland','israel','canada','Kazakhstan','kenya','colombia','Costa rica','Morocco','Mexico','rusian','peru','chile','pakistan','turki','perancis','jerman','qatar','Venezuela','vietnam','myanmar','Papua New Guinea','taiwan','yemen','new Zealand','nepal','Madagascar','thailand','mali','paraguay','Luksemburg','palestine','Latvia','jordan','somalia','Equatorial Guinea','Nicaragua','maldives','Lebanon','cuba','Mozambique','Macao','Egypt','Mongolia','nauru','oman','greenland']
  console.log(item.length)
  var counter = 0;
 var x = setInterval(function(){
  // var i = item[Math.floor(Math.random() * item.length )]
  if(counter==item.length){
    $(".kartu").css("opacity",1)
+   $(".c-footer").css("opacity",1)
    clearInterval(x);
    jd.innerHTML=("Data dari "+counter+" Negara")
  }else{
@@ -21,7 +22,7 @@ $.getJSON(`https://disease.sh/v3/covid-19/countries/${i}`,function(data){
   $("#daftar").append(`<div class="col md-15"><div class="card kartu mb-3" id="${data.country}"><img src="${bendera}" class="card-img-top" alt="${data.country}"><div class="card-body"><h5 class="card-title">${data.country}</h5><p>Kasus : ${kasus.toLocaleString()}</p><p>kematian : ${kematian.toLocaleString()}</p><p>sembuh : ${sembuh.toLocaleString()}</p><p class="card-text">populasi : ${populasi.toLocaleString()}</p></div></div></div>`)
 });
 }
-},)
+},100)
 
 function fetch(){
   let i = document.getElementById('input').value;
@@ -30,13 +31,11 @@ function fetch(){
     alert("404\nnegara ["+i+"] tidak ditemukan !")
   }else{
   document.location.href=`#${i}`
-  apa.style.position="relative"
-  apa.style.backgroundColor="yellow"
-  apa.style.boxShadow="0px 0px 15px black"
+ /* apa.style.position="relative"
+  apa.style.backgroundColor="lightblue"
+  // apa.style.boxShadow="0px 0px 15px black"
 setTimeout(function(){
-  apa.style.position="static"
-  apa.style.backgroundColor="#fff"
-  apa.style.boxShadow="none"
-},3000)
+  apa.style.backgroundColor=""
+},3000) */
 }
 }
