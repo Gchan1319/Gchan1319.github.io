b8b0c79e75039bfa5a45707c4c676964f702c4bc
Gchan1319 <83627675+Gchan1319@users.noreply.github.com>
2021-08-20 19:23:25 +0700

    Add files via upload

--- newGchan.js
+++ newGchan.js
@@ -19,37 +19,37 @@ function pilih(){
     case 'google':
       fr.action="https://google.com/search";
       i.name="q";
-      i.placeholder="Cari di Google";
+      i.placeholder="Search On Google";
       break;
     case 'duck':
       fr.action="https://duckduckgo.com/?";
       i.name="q";
-      i.placeholder="Cari di DuckDuckGo";
+      i.placeholder="Search On DuckDuckGo";
       break;
     case 'yahoo':
       fr.action="https://id.search.yahoo.com/search";
       i.name="q";
-      i.placeholder="Cari di Yahoo";
+      i.placeholder="Search On Yahoo";
       break;
     case 'yandex':
       fr.action="https://yandex.com/search/touch?";
       i.name="text";
-      i.placeholder="Cari di Yandex";
+      i.placeholder="Search On Yandex";
       break;
     case 'bing':
       fr.action="https://bing.com/search";
       i.name="q";
-      i.placeholder="Cari di Bing"
+      i.placeholder="Search di Bing"
       break;
     case 'youtube':
       fr.action="https://m.youtube.com/results";
       i.name="search_query";
-      i.placeholder="Cari di YouTube";
+      i.placeholder="Search di YouTube";
       break;
     case 'playstore':
       fr.action = "https://play.google.com/store/search";
       i.name = "q";
-      i.placeholder = "Cari di Playstore";
+      i.placeholder = "Search On Playstore";
       break;
     case 'ecosia':
       fr.action = "https://www.ecosia.org/search";
@@ -59,17 +59,17 @@ function pilih(){
     case 'ask':
         fr.action = "https://www.ask.com/web";
         i.name = "q";
-        i.placeholder = "Cari di Ask";
+        i.placeholder = "Search On Ask";
       break;
     case 'kusonime':
       fr.action="https://kusonime.com/";
       i.name="s";
-      i.placeholder="Cari di Kusonime";
+      i.placeholder="Search On Kusonime";
       break;
       case 'samehadaku':
           fr.action = "https://samehadaku.sbs/";
           i.name = "s";
-          i.placeholder = "Cari di Samehadaku";
+          i.placeholder = "Search On Samehadaku";
         break;
       
   }
@@ -97,30 +97,42 @@ function login_send(){
   user.innerHTML=namaR;
   age.innerHTML=umurP;
   logo.src="img/gw.jpg";
+  $.getJSON("https://api.db-ip.com/v2/free/self",function(data){
+    console.log(data)
+    let ip = data.ipAddress;
+    let country = data.countryName
+    $("#ip").append(`${ip} From ${country}`);
+  })
+  let device = navigator.userAgent;
+  $("#device").append(`${device}`)
   }else{
     nama.classList.toggle("is-invalid");
     umur.classList.toggle("is-invalid");
-    nama.placeholder="Masukan Nama !"
-    umur.placeholder="Masukan Umur !"
+    nama.placeholder="Enter Your Name !"
+    umur.placeholder="Enter Your Age !"
   }
 }
 let audio = document.getElementById("music")
 function mplay(){
   audio.play();
   audio.loop=true;;
-  $(".music img").attr("src","img/music.png");
-  $(".music").attr("onclick","pauseMusic()");
+  $(".musicbtn img").attr("src","img/music.png");
+  $(".musicbtn").attr("onclick","pauseMusic()");
+  $(".musicbtn").addClass("music");
   var x = setInterval(warnai,100);
 }
 function pauseMusic(){
   audio.pause();
-  $(".music img").attr("src","img/music-mute.png");
-  $(".music").attr("onclick","mplay()")
+  $(".musicbtn").css("background","#0BF3C5")
+  $(".musicbtn img").attr("src","img/music-mute.png");
+  $(".musicbtn").attr("onclick","mplay()")
+  $(".musicbtn").toggleClass("music");
+  $(".musicbtn").addClass("musicmuted");
 }
 function warnai(){
     var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     var res = randomColor;
-    $(".music").css("background-color",res);
+    $(".music").css("background",res);
 }
 /*
 function youtube(){
