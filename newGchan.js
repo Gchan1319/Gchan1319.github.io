ca3b23019394889478e32c027a1b7a9d73ca7074
Gchan1319 <83627675+Gchan1319@users.noreply.github.com>
2021-10-28 16:10:34 +0700

    Add files via upload

--- newGchan.js
+++ newGchan.js
@@ -1,16 +1,47 @@
+var colorSchemeDark = matchMedia('(prefers-color-scheme: dark)').matches;
 $("#tema").attr("onclick",`ganti(${true})`);
 $("#logo").attr("src","img/UserNone-light.png");
-var cookie = Cookies.get("tema");
-if(cookie != null){
-  if(cookie == "dark"){
+var IsOnline = navigator.onLine;
+IsOnline = false
+var meta = document.querySelector("meta[name=theme-color]");
+var cookie_theme = Cookies.get("tema");
+var IsAlreadyLogin = Cookies.get("AlreadyLogin");
+// check if device scheme is dark
+if(colorSchemeDark){
+  ganti(true);
+  $("#tema").attr("onclick",`ganti(${false})`);
+}else{
+  // if not dark 
+  IsDarkMode();
+}
+if(IsAlreadyLogin == "true"){
+  let username = Cookies.get("username");
+  let age = Cookies.get("user_age");
+  notif("welcome back "+username);
+  autoLogin(username,age);
+}
+
+//console.log(typeof IsAlreadyLogin)
+function IsDarkMode(){
+  if(cookie_theme == "dark"){
     ganti(true);
     $("#tema").attr("onclick",`ganti(${false})`);
+  }else{
+    ganti(false);
+    $("#tema").attr("onclick",`ganti(${true})`);
   }
 }
 var isLogin = false;
 function ganti(isDark){
   if(isDark){
+  meta["content"] = "rgb(33,37,41)";
+  Cookies.remove("tema");
+  $("#notif").removeClass("blend");
+  $("#notif").addClass("bg-dark");
   Cookies.set("tema","dark");
+  notif("Success Change To Dark");
+  $("select").addClass("bg-dark");
+  $("select").addClass("text-light");
   $("#tema").removeClass("moon");
   $("#tema").attr("onclick",`ganti(${false})`);
   $("#tema").addClass("sun");
@@ -29,6 +60,7 @@ function ganti(isDark){
   $(".ig").removeClass("ig-light");
   $(".fb").addClass("fb-dark");
   $(".ig").addClass("ig-dark");
+  
   if(!isLogin){
     $("#logo").attr("src","img/UserNone-dark.png");
   }
@@ -36,7 +68,13 @@ function ganti(isDark){
   if(!isLogin){
   $("#logo").attr("src","img/UserNone-light.png");
   }
+  meta["content"] = "rgba(17,255,225,1)";
   Cookies.remove("tema");
+  Cookies.set("tema","light");
+  $("#notif").addClass("blend");
+  $("#notif").removeClass("bg-dark");
+  $("select").removeClass("text-light");
+  $("select").removeClass("bg-dark");
   $("#tema").removeClass("sun");
   $("#tema").attr("onclick",`ganti(${true})`);
   $("#tema").addClass("moon");
@@ -55,7 +93,9 @@ function ganti(isDark){
   $(".ig").addClass("ig-light");
   $(".fb").removeClass("fb-dark");
   $(".ig").removeClass("ig-dark");
+  notif("Success Change To Light");
   }
+  
 }
 function GetWaifu(){
   $.getJSON("https://api.waifu.pics/sfw/waifu",function(data){
@@ -73,9 +113,10 @@ function GetWaifu(){
 function zoom(){
   $("#content").addClass("zooming");
 }
-GetWaifu();
-var x = setInterval(GetWaifu,15000);
+
 function pilih(){
+  let fr = document.getElementById("fr");
+  let i = document.getElementById("i").value;
   let a = inputGroupSelect01.value
   switch (a) {
     case 'google':
@@ -101,12 +142,12 @@ function pilih(){
     case 'bing':
       fr.action="https://bing.com/search";
       i.name="q";
-      i.placeholder="Search di Bing"
+      i.placeholder="Search On Bing"
       break;
     case 'youtube':
       fr.action="https://m.youtube.com/results";
       i.name="search_query";
-      i.placeholder="Search di YouTube";
+      i.placeholder="Search On YouTube";
       break;
     case 'playstore':
       fr.action = "https://play.google.com/store/search";
@@ -115,7 +156,7 @@ function pilih(){
       break;
     case 'ecosia':
       fr.action = "https://www.ecosia.org/search";
-      i.placeholder = "Cari di Ecosia";
+      i.placeholder = "Search On Ecosia";
       i.name = "q";
       break;
     case 'ask':
@@ -133,76 +174,172 @@ function pilih(){
           i.name = "s";
           i.placeholder = "Search On Samehadaku";
         break;
-      
+      default :
+      i.placeholder = "Search On Google";
+      break;
   }
+}
+/*
+function command(i){
+  //let i = document.getElementById("command").value;
+  let pp = i.slice(0,6);
+  let prefix = "/";
+  switch(i){
+    case `${prefix}setpp`:
+      let url = prompt("input url !");
+      $("#logo").attr("src",`${url}`);
+      notif("Logo Changed !");
+      break;
+    case `${prefix}set dark`:
+      ganti(true);
+      notif("Success Change To Dark");
+      break;
+    case `${prefix}set light`:
+      ganti(false);
+      notif("Success Change To Light");
+      break;
+    case `${prefix}clock`:
+      let now = new Date();
+      let jam = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+      notif(`${jam}`);
+      break;
+    default:
+    console.log("default");
+    notif("Command Not Found !");
+      break;
+  }
+  i.value = "";
+}
+*/
+function notif(text,time){
+  $("#notif").html(text);
+  $("#notif").css("transform","translateY(0)");
+  if(time == null){
+  time = 1500;
+  setTimeout(() => {
+    $("#notif").css("transform","translateY(-200%)");
+  },time)
+  }else{
+    setTimeout(() => {
+      $("#notif").css("transform", "translateY(-200%)");
+    }, time);
+  }
+  // return "sukses send a notify "+text+" For "+time/1000+" second !";
 }
 function cek(){
   $("#login_s").css("max-height","0");
 }
 $("#hero").ready(function(){
-  hero.style.display="block";
+  $("#hero").css("display","block")
 })
 $(document).ready(function(){
-  preload.style.display="none";
+  $("#preload").css("display","none");
+  $("#preload").remove();
 })
+
 function login(){
   $("#login_s").css("max-height","100%");
-  $("#login_s").toggleClass("sctop")
+  $("#login_s").toggleClass("sctop");
 }
-
+var nama = document.getElementById("nama");
+var umur = document.getElementById("umur");
 function login_send(){
   let info = $.getJSON("log.json",function(data){
-  let username = data.username
-  if(nama.value == username){
+  let username = data.username;
+  if(nama.value == username && umur.value != "" && umur.value == 16){
     $("#login_s").css("max-height","0");
     $("#L1").addClass("d-none");
     $("#L2").toggleClass("d-none");
     let nameP = nama.value;
     let umurP = umur.value;
     let namaR = nameP.slice(0, 27);
-    user.innerHTML = "Admin"
-    age.innerHTML = umurP;
+    if(namaR.length > 30){
+    $("#user").addClass("mrq");
+    }else{
+    $("#user").addClass("d-inline");
+    }
+    $("#user").html("Admin");
+    $("#age").html(umurP);
     $("#fr").attr("onclick","");
     $("#logo").attr("src","img/gw.jpg");
-     $.getJSON("https://api.db-ip.com/v2/free/self", function(data) {
-       console.log(data)
-       let ip = data.ipAddress;
-       $("#music").attr("src","sound/MyWar.mp3");
+    $.getJSON("https://api.db-ip.com/v2/free/self", function(data) {
+      // console.log(data)
+      let ip = data.ipAddress;
+      let country = data.countryName
+      $("#ip").append(`${ip} From ${country}`);
+    }).catch(Error => { $("#ip").append(`unknown`) })
+     $("#music").attr("src","sound/MyWar.mp3");
        $("#body").append(`<div class="musicbtn blend musicmuted" onclick="mplay()"><img src="img/music-mute.png" class="img-fluid rounded rounded-circle" alt="playButton" /></div>`);
-       let country = data.countryName
-       $("#ip").append(`${ip} From ${country}`);
-     })
      isLogin = true;
      let piranti = navigator.userAgent;
      $("#device").append(`${piranti}`);
-  }else if(nama.value != "" && umur.value != ""){
+   Cookies.set("AlreadyLogin",true);
+   Cookies.set("username","Admin");
+   Cookies.set("user_age",umurP);
+  }else if(nama.value != "" && umur.value != "" && umur.value <= 100){
   $("#login_s").css("max-height","0");
   $("#L1").addClass("d-none");
   $("#L2").toggleClass("d-none");
   let nameP = nama.value;
   let umurP = umur.value;
-  let namaR = nameP.slice(0, 27);
-  user.innerHTML=namaR;
-  age.innerHTML=umurP;
+  let namaR = nameP;
+  $("#user").html(namaR);
+  if(namaR.length > 30){
+    $("#user").addClass("mrq");
+  }else{
+    $("#user").addClass("d-inline");
+  }
+  //console.log(namaR.length)
+  $("#age").html(umurP);
   $("#logo").attr("src","img/gw.jpg");
    $.getJSON("https://api.db-ip.com/v2/free/self", function(data) {
-     console.log(data)
+     // console.log(data)
      let ip = data.ipAddress;
      let country = data.countryName
      $("#ip").append(`${ip} From ${country}`);
-   })
+   }).catch(Error => { $("#ip").append(`unknown`) })
    $("#music").attr("src","sound/MyWar.mp3");
    $("#body").append(`<div class="musicbtn musicmuted" onclick="mplay()"><img src="img/music-mute.png" class="img-fluid rounded rounded-circle" alt="playButton" /></div>`);
    isLogin = true;
    let piranti = navigator.userAgent;
    $("#device").append(`${piranti}`);
+   Cookies.set("AlreadyLogin",true);
+   Cookies.set("username",namaR);
+   Cookies.set("user_age",umurP);
   }else{
     nama.classList.toggle("is-invalid");
     umur.classList.toggle("is-invalid");
-    nama.placeholder="Enter Your Name !"
-    umur.placeholder="Enter Your Age !"
+    nama.placeholder="Enter Your Name !";
+    umur.placeholder="Enter Your Age !";
+    
   }
   });
+}
+function autoLogin(nama,umur) {
+  $("#login_s").css("max-height","0");
+  $("#L1").addClass("d-none");
+  $("#L2").toggleClass("d-none");
+  isLogin = true;
+  $("#user").html(nama);
+  if(nama.length > 30){
+    $("#user").addClass("mrq");
+  }else{
+    $("#user").addClass("d-inline");
+  }
+  //console.log(namaR.length)
+  age.innerHTML=umur;
+  $("#logo").attr("src","img/gw.jpg");
+   $.getJSON("https://api.db-ip.com/v2/free/self", function(data) {
+     // console.log(data)
+     let ip = data.ipAddress;
+     let country = data.countryName
+     $("#ip").append(`${ip} From ${country}`);
+   }).catch(Error => { $("#ip").append(`unknown`) })
+   $("#music").attr("src","sound/MyWar.mp3");
+   $("#body").append(`<div class="musicbtn musicmuted" onclick="mplay()"><img src="img/music-mute.png" class="img-fluid rounded rounded-circle" alt="playButton" /></div>`);
+   isLogin = true;
+   let piranti = navigator.userAgent;
+   $("#device").append(`${piranti}`);
 }
 let audio = document.getElementById("music")
 function mplay(){
@@ -227,6 +364,34 @@ function warnai(){
     var res = randomColor;
     $(".music").css("background",res);
 }
+if (IsOnline) {
+  GetWaifu();
+  var x = setInterval(GetWaifu,15000)
+} else {
+  $("#hero").attr("alt","You are Offline !");
+  $("#sc").attr("href","#");
+  //$("#fr,#searchMenu").remove();
+  $("#sc").attr("target","");
+  
+}
+/*
+var cBtnNav = 0;
+$("#fr").on("click",function(){
+ let x = setInterval(fbi,100);
+ if(cBtnNav == 1){
+   clearInterval(x);
+   cBtnNav = 0;
+ }else{
+   cBtnNav++;
+ }
+})
+*/
+/* Change Source Music */
+/*
+function changeMusic(args){
+  $("#music").attr("src",`${args}`);
+}
+*/
 /*
 function youtube(){
   let text = i.value
